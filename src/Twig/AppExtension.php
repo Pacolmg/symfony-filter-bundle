ad508750991ad23dcbfcfea5e374c93e5168d493
Paco <paco.morcillo@solucionex.com>
2019-06-28 19:14:41 +0200

    TwigFilter to encode the filters of the template

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Pacolmg\SymfonyFilterBundle\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AppExtension extends AbstractExtension
+{
+    /**
+     * @return array
+     */
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('filters_to_path', [$this, 'filtersToPath'])
+        ];
+    }
+
+    /**
+     * Encode the filters in order to concatenate them after a url
+     *
+     * @param $queryParams
+     * @param $filters
+     * @return string
+     */
+    public function filtersToPath($queryParams, $filters)
+    {
+        $filters = array_merge($filters, ['page', 'limit']);
+        $filterPath = '';
+        foreach ($filters as $filter) {
+            if (isset($queryParams[$filter])) {
+                $filterPath = sprintf('%s%s', $filterPath, sprintf('&%s=%s', $filter, is_array($queryParams[$filter]) ? join(',', $queryParams[$filter]) : $queryParams[$filter]));
+            }
+        }
+
+        return $filterPath;
+    }
+}
