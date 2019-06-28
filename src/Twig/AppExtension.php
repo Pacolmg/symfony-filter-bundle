52e372c7f686c295f96e7e55490b092a46e7ff8f
Paco <paco.morcillo@solucionex.com>
2019-06-28 19:36:29 +0200

    Add phpdoc

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -5,6 +5,13 @@ namespace Pacolmg\SymfonyFilterBundle\Twig;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
+/**
+ * Class AppExtension
+ *
+ * @package Pacolmg\SymfonyFilterBundle\Twig
+ * @author Pacolmg <pacolmg@gmail.com>
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
 class AppExtension extends AbstractExtension
 {
     /**
